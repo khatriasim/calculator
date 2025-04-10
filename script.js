2c4093c3f5b827c37dbdf7da95d4ede623955098
khatriasim <khatriasim111@gmail.com>
2025-04-10 15:30:16 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let numbers = document.querySelector(".numbers")
+let number = document.querySelector(".number")
+
+ let button1 = document.createElement("button")
+ button1.textContent = "1"
+ button1.className = "button"
+number.appendChild(button1)
+
+let button2 = document.createElement("button")
+button2.textContent = "2"
+button2.className = "button"
+number.appendChild(button2)
+
+let button3 = document.createElement("button")
+button3.textContent = "3"
+button3.className = "button"
+number.appendChild(button3)
+
+let button4 = document.createElement("button")
+button4.textContent = "4"
+button4.className = "button"
+number.appendChild(button4)
+
+let button5 = document.createElement("button")
+button5.textContent = "5"
+button5.className = "button"
+number.appendChild(button5)
+
+let button6 = document.createElement("button")
+button6.textContent = "6"
+button6.className = "button"
+number.appendChild(button6)
+
+let button7 = document.createElement("button")
+button7.textContent = "7"
+button7.className = "button"
+number.appendChild(button7)
+
+let button8 = document.createElement("button")
+button8.textContent = "8"
+button8.className = "button"
+number.appendChild(button8)
+
+let button9 = document.createElement("button")
+button9.textContent = "9"
+button9.className = "button"
+number.appendChild(button9)
+
+let button0 = document.createElement("button")
+button0.textContent = "0"
+button0.className = "button0"
+number.appendChild(button0)
